3274ff4dc4489c0ef45ecbda3fba79067eedae06
rinormaloku <rinormaloku37@gmail.com>
2016-07-31 13:51:30 +0200

    * Added polymer element to the plugin/extension HTML. * Updated JS files to contain the logic that disables clicking like and replaces 'Like'-s with 'Hate'-s * Updated manifest.json to provide permission for storage.

--- js/applyOnExtension.js
+++ js/applyOnExtension.js
@@ -3,11 +3,27 @@
  */
 
 window.onload = function () {
-    var initButton = document.getElementById('initConsoleMessage');
+    var hateToggler = document.getElementById('hateToggler');
 
-    initButton.addEventListener("click", function () {
+
+    /*
+    * Reads last state of hate. and sets the UI of the plugin
+    * */
+    chrome.storage.local.get("hateTogglerState", function (result) {
+        hateToggler.checked = result.hateTogglerState;
+    });
+
+
+    /*
+    * This event listener saves changes of the state to chrome local storage and
+    * sends turnHateOn message which will be listened by applyOnPage.js (content script)
+    * */
+    hateToggler.addEventListener("click", function () {
+        var updatedHateState = hateToggler.checked;
+        chrome.storage.local.set({hateTogglerState: updatedHateState}, function () {});
+
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "executeConsoleMessage"});
+            chrome.tabs.sendMessage(tabs[0].id, {action: "turnHateOn"});
         });
     });
 };
--- js/applyOnPage.js
+++ js/applyOnPage.js
@@ -1,8 +1,50 @@
+/*
+* This script is executed when match is made ->
+* If page in domain facebook.com/* then show the extension
+* */
 chrome.runtime.sendMessage({action: "show"});
 
+function changeAllLikesTo(text) {
+    var likesToHate = document.querySelectorAll('a[data-testid="fb-ufi-likelink"]');
+    for (var i = 0; i < likesToHate.length; i++) {
+        likesToHate[i].text = text;
+    }
+}
 
+function hate() {
+    changeAllLikesTo('Hate');
+}
+
+var mainContent = document.querySelector("div[id^='topnews_main_stream']");
+var config = {childList: true, subtree: true};
+var observer = new MutationObserver(function (mutations) {
+    hate();
+});
+
+function setUpLikeAbility() {
+    chrome.storage.local.get("hateTogglerState", function (result) {
+        if (result.hateTogglerState) {
+            document.styleSheets[0].insertRule('a[data-testid="fb-ufi-likelink"]{pointer-events: none !important;}', 0);
+
+        } else {
+            document.styleSheets[0].removeRule(0);
+        }
+        hate();
+        observer.observe(mainContent, config);
+    });
+}
+
+/*
+* Call to set up the state according to last settings.
+* Meaning: Extreme hate on or off.
+* */
+setUpLikeAbility();
+
+/*
+* Listener for messages from applyOnExtension.js
+* */
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action == 'executeConsoleMessage') {
-        console.debug('Button clicked message triggered');
+    if (request.action == 'turnHateOn') {
+        setUpLikeAbility();
     }
 });
